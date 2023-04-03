73bd01b8026afd74987335a22d3c54189d202c6c
Soumya Nagula <19tr1a0576@scit.ac.in>
2023-04-03 15:47:37 +0530

    portfolio created successfully

---
+++ project1/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Aboutme from './pages/Aboutme';
+import Qualifications from './pages/Qualifications';
+import Hobbies from './pages/Hobbies';
+import Skills from './pages/Skills';
+import Experience from './pages/Experience';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="Aboutme" element={<Aboutme/>}/>
+          <Route path="Qualifications" element={<Qualifications/>}/>
+          <Route path="Hobbies" element={<Hobbies/>}/>
+          <Route path="Skills" element={<Skills/>}/>
+          <Route path="Experience" element={<Experience/>}/>
+
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ project1/src/pages/Aboutme/index.js
@@ -0,0 +1,14 @@
+import { Typography } from '@mui/material';
+
+export default function Aboutme(){
+    return(
+        <>
+<Typography>Name: SOWMYA NAGULA</Typography>
+<Typography>Father Name: SRINIVAS</Typography>
+<Typography>Mother Name: SUMATHI</Typography>
+<Typography>Email: sowmyanagula@gmail.com</Typography>
+<Typography>Phone No.: 7780483738</Typography>
+        </>
+        
+    )
+    }
